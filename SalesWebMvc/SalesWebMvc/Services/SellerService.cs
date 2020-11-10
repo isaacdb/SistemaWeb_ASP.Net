2f6b7de7343746118cba1cd4e936a2101f701452
Isaac <isaacdbs@outlook.com>
2020-11-10 15:35:34 -0300

    Adicionado department Id para criacao de vendededores

--- SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -3,6 +3,7 @@ using SalesWebMvc.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
