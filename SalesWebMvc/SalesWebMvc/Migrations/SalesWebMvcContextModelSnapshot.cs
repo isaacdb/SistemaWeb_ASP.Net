139bbc1b8b6cd1b12cf682290f4d6d0d1bd93478
Isaac <isaacdbs@outlook.com>
2020-11-11 16:37:06 -0300

    PrintScreens

--- SalesWebMvc/SalesWebMvc/Migrations/SalesWebMvcContextModelSnapshot.cs
+++ SalesWebMvc/SalesWebMvc/Migrations/SalesWebMvcContextModelSnapshot.cs
@@ -65,9 +65,12 @@ namespace SalesWebMvc.Migrations
 
                     b.Property<int>("DepartmentId");
 
-                    b.Property<string>("Email");
+                    b.Property<string>("Email")
+                        .IsRequired();
 
-                    b.Property<string>("Name");
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(60);
 
                     b.HasKey("Id");
 
